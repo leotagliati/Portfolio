1b7f0aa528d8e789fc9e5ec0562811523e0d7ed9
Leonardo Tagliati <leotagliati56@gmail.com>
2024-12-29 11:05:45 -0300

    first c ommit

---
+++ WebsiteTest/src/components/About.jsx
@@ -0,0 +1,19 @@
+function About(){
+    return (
+        <>
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
+                <div className="p-4 w-full bg-red-500">
+                    <img src="src\assets\img\project1Image.png" alt="" />
+                </div>
+                <div>
+                    <h6>Hello, I'm Leo</h6>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fugiat ad consequuntur sed, harum veniam magni qui, est incidunt adipisci nobis tempora, quasi dolore repellat vel distinctio accusantium totam voluptatem?
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default About;
---
+++ WebsiteTest/src/components/Intro.jsx
@@ -0,0 +1,9 @@
+function Intro(){
+    return (
+        <>
+            <h1>That's an Intro!</h1>
+        </>
+    );
+}
+
+export default Intro;
