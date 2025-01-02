b22b069201a0f10871dda628c338aa6142f6e18d
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-02 17:16:51 -0300

    profilepic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,11 @@ function About() {
     return (
         <>
             <div className="bg-purple-leo my-10 grid gap-4 grid-rows-2">
-                <div className="flex p-4 w-full justify-center">
-                    <img className="" src="src\assets\img\profileImage.png" alt="" />
+                <div className="flex  justify-center">
+                    <img className="mt-12 h-48" src="src\assets\img\profileImage.png" alt="" />
                 </div>
                 <div className="p-3">
-                    <h6 className="mt-12 mr-3 text-center text-cream-leo  h-14 text-5xl font-pixelify">Hi there, I'm Leo!</h6>
+                    <h6 className="mt-6 mr-3 text-center text-cream-leo  h-14 text-5xl font-pixelify">Hi there, I'm Leo!</h6>
                     <p className="my-5 mr-3 text-center text-cream-leo  font-pixelify text-3xl">
                     I'm a software engineer based in Brazil, specialized in 
                     </p>
