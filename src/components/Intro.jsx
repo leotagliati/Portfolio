f2f5827e60d251a0072c1e40075df5549fdd2379
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-03 15:07:46 -0300

    portfoilio capslock

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,7 @@ function Intro() {
     return (
         <>
             <div className="my-10 z-10 relative">
-                <h1 className="p-48 text-8xl text-center  text-cream-leo font-pacifico">PORTFOLIO</h1>
+                <h1 className="p-48 text-8xl text-center  text-cream-leo font-pacifico">Portifolio</h1>
             </div>
         </>
     );
