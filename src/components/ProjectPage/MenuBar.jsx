fa66d6fce2297e35cee6b5d3fb04ed87bfff4377
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-03 16:58:43 -0300

    project-title-estrutura

--- src/components/ProjectPage/MenuBar.jsx
+++ src/components/ProjectPage/MenuBar.jsx
@@ -3,11 +3,11 @@ import ContactIcon from "../ContactIcon";
 function MenuBar() {
     return (
         <>
-            <div className=" w-full py-8 h-20 bg-purple-500">
-                <div className="bg-red-800 mx-48 h-12 text-2xl flex flex-row justify-center gap-12">
-                    <h1 className="mt-4">Home</h1>
-                    <h1 className="mt-4">Portifolio</h1>
-                    <h1 className="mt-4">Contact</h1>
+            <div className=" w-full py-8 h-20 bg-purple-500 overflow-hidden">
+                <div className="bg-red-800 rounded-3xl my-3 mx-52 h-16 text-2xl flex flex-row justify-center gap-12 relative z-1 overflow-hidden">
+                    <h1 className="mt-1">Home</h1>
+                    <h1 className="mt-1">Portifolio</h1>
+                    <h1 className="mt-1">Contact</h1>
                 </div>
             </div>
         </>
--- src/components/ProjectPage/ProjectTitle.jsx
+++ src/components/ProjectPage/ProjectTitle.jsx
@@ -2,8 +2,12 @@ function ProjectTitle({ name, id }) {
     let imgPath1 = `./src/assets/img/${id}-image1.png`;
     return (
         <>
-            <div className="w-72 h-96 bg-cyan-200 ">
-                <h1 className=" text-2xl text-center bg-red-600 text-slate-100">{name}</h1>
+            <div className="w-1/3 bg-cyan-200 grid grid-rows-2 gap-6 p-4 justify-center">
+                <img src="src\assets\img\title-sword-of-obsessia.png" alt="" className="bg-green-300 py-8" />
+                <div className="grid grid-cols-2 py-8 px-6">
+                    <img src="src\assets\img\Logo-icon.png" alt="" className="bg-green-300 " />
+                    <h1 className="bg-red-500 text-center text-2xl mt-6 h-20">Developed by <h2 className="font-extrabold">Pluma Studios</h2></h1>
+                </div>
             </div>
         </>
     );
