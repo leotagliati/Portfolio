9bc008849b8affe879e37c324480b9a46d5767f4
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-03 10:12:36 -0300

    setup para efeito parllax

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,12 +1,15 @@
 function Parallax() {
     return (
-        <div className="relative ">
-            <img src="src\assets\img\Parallax\Parallax1.png" alt="" className="w-full absolute" />
-            <img src="src\assets\img\Parallax\Parallax2.png" alt="" className="w-full absolute" />
-            <img src="src\assets\img\Parallax\Parallax3.png" alt="" className="w-full absolute translate-y-48" />
-            <img src="src\assets\img\Parallax\Parallax4.png" alt="" className="w-full absolute translate-y-64" />
-            <img src="src\assets\img\Parallax\Parallax5.png" alt="" className="w-full absolute translate-y-96" />
-        </div>
+        <>
+            <div className="relative ">
+                <img src="src\assets\img\Parallax\Parallax1.png" alt="" id="stars" className="w-full absolute" />
+                <img src="src\assets\img\Parallax\Parallax2.png" alt="" id="smoke1" className="w-full absolute" />
+                <img src="src\assets\img\Parallax\Parallax3.png" alt="" id="smoke2" className="w-full absolute translate-y-48" />
+                <img src="src\assets\img\Parallax\Parallax4.png" alt="" id="smoke3" className="w-full absolute translate-y-64" />
+                <img src="src\assets\img\Parallax\Parallax5.png" alt="" id="mountains" className="w-full absolute translate-y-96" />
+            </div>
+            
+        </>
     );
 }
 export default Parallax;
