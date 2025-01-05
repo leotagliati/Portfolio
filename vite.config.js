ffc7e9807791414a479dc5090599f2e2e86927aa
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-05 12:29:04 -0300

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/',
   plugins: [react()],
   css:{
     postcss:{
