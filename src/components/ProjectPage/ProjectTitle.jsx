1d288d7019634e2b3099a22f1008f7865487da8d
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-07 11:13:02 -0300

    title resolvido

--- src/components/ProjectPage/ProjectTitle.jsx
+++ src/components/ProjectPage/ProjectTitle.jsx
@@ -6,9 +6,9 @@ function ProjectTitle({ id, studioId }) {
             <div className="w-1/3 border-y border-black  bg-gradient-to-b from-dark-gray-navy to-black-navy grid md:grid-rows-2 gap-6 p-4 justify-center">
                 <img src={imgTitlePath} alt={`title-${id}`} className="py-8" />
                 {studioId != null ? 
-                    <div className="grid md:grid-cols-2 py-8 px-6">
+                    <div className="grid grid-cols-2 md:py-8 md:px-6">
                         <img src={imgPathLogo} alt="" className="" />
-                        <h1 className="text-center text-clay-navy md:text-2xl mt-8 h-20">Developed by <h2 className="font-extrabold">{formatStudioName(studioId)}</h2></h1>
+                        <h1 className="text-center text-clay-navy md:text-2xl md:mt-8 h-20">Developed by <h2 className="font-extrabold">{formatStudioName(studioId)}</h2></h1>
                     </div>
                     :
                     <div className="grid md:grid-cols-2 py-8 px-6">
