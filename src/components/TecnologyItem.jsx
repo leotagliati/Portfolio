7aedb8e1b6c96f0c00f5a53d883d445a86aa3a1d
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-08 14:44:30 -0300

    fixes font

--- src/components/TecnologyItem.jsx
+++ src/components/TecnologyItem.jsx
@@ -10,8 +10,8 @@ function TecnologyItem({ title, imgNames, description }) {
                     ))}
                 </div>
                 <div className="  w-full h-32  left-0 bottom-0 right-0">
-                    <h2 className="mx-1 mt-2 text-3xl text-cream-leo font-pixelify ">{title}</h2>
-                    <p className="mx-1 mt-1 sm:text-2xl md:text-3xl line-clamp-5 text-cream-leo font-pixelify">{description}</p>
+                    <h2 className="mx-1 mt-2 sm:text-3xl md:text-4xl text-cream-leo font-lato underline underline-offset-8 decoration-cream-leo decoration-4">{title}</h2>
+                    <p className="mx-2 mt-4 sm:text-2xl md:text-3xl line-clamp-5 text-cream-leo font-inter">{description}</p>
                 </div>
             </div>
         </div>
