3fdf93c13eb7148559d0b8b0fbffff7138851de8
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-08 14:33:18 -0300

    project-page-3 estrutura

--- src/Pages/Project3Page.jsx
+++ src/Pages/Project3Page.jsx
@@ -5,7 +5,7 @@ import ProjectTitle from "../components/ProjectPage/ProjectTitle";
 import { useRef, useEffect, useLayoutEffect } from "react";
 import TeamMembers from "../components/ProjectPage/TeamMembers";
 import VideoShowcase from "../components/ProjectPage/VideoShowcase";
-import {useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Project3Page() {
     const background = useRef(null);
@@ -13,7 +13,7 @@ function Project3Page() {
 
     // scroll to top of page after a page transition.
     useLayoutEffect(() => {
-        document.documentElement.scrollTo({ top:0, left:0, behavior: "instant" });
+        document.documentElement.scrollTo({ top: 0, left: 0, behavior: "instant" });
     }, [location.pathname])
 
     useEffect(() => {
@@ -33,10 +33,10 @@ function Project3Page() {
     return (
         <>
 
-            <img id="background" src="img/sword-of-obsessia-background.png" alt="" ref={background} className="w-full absolute z-0" />
+            <img id="background" src="img/website-portfolio-background.png" alt="" ref={background} className="w-full absolute z-0" />
             <div className="w-full h-full relative z-1">
                 <MenuBar></MenuBar>
-                <ProjectTitle id="sword-of-obsessia" studioId="pluma-studios"></ProjectTitle>
+                <ProjectTitle id="website-portfolio" ></ProjectTitle>
 
 
                 {/* Page Description */}
@@ -47,11 +47,11 @@ function Project3Page() {
                         <div className="h-96">
                             <div className="">
                                 <div className=" pb-2 border-b-2 w-fit">
-                                    <h2 className="text-4xl text-clay-navy font-bold font-jetbrains">Sinopse</h2>
+                                    <h2 className="text-4xl text-clay-navy font-bold font-jetbrains">Descrição</h2>
                                 </div>
                             </div>
                             <div className="mt-3 ">
-                                <h2 className="text-left text-3xl text-clay-navy font-jetbrains">Em Sword of Obsessia, você embarca em uma jornada épica como um cavaleiro com uma espada amaldiçoada. Atravessando masmorras, você enfrentará seus medos e jogará dados valendo seu destino!</h2>
+                                <h2 className="text-left text-3xl text-clay-navy font-jetbrains">Este projeto em React é uma ideia minha para aprimorar minha expertise em frontend. O objetivo é criar um portfólio que não apenas demonstre meus projetos, mas também reflita minha atenção aos detalhes e habilidades em criar interfaces intuitivas e visualmente atraentes.</h2>
                             </div>
                         </div>
 
@@ -59,15 +59,15 @@ function Project3Page() {
                         <div className="mt-12 table h-96">
                             <div className="table-row ">
                                 <div className="pb-2 border-b-2 w-fit">
-                                    <h2 className="text-4xl text-clay-navy font-bold">Responsabilidades</h2>
+                                    <h2 className="text-4xl text-clay-navy font-bold">Habilidades</h2>
                                 </div>
                             </div>
                             <div className="table-row">
-                                <h2 className="mt-3 text-3xl line-clamp-3 text-clay-navy font-jetbrains">Durante o desenvolvimento, assumi diversas responsabilidades fundamentais:</h2>
+                                <h2 className="mt-3 text-3xl line-clamp-3 text-clay-navy font-jetbrains">Durante o desenvolvimento:</h2>
                                 <ul className="list-disc pt-3 pl-6 text-2xl font-jetbrains">
-                                    <li className="text-clay-navy mt-3">Desenvolvi sistemas de combate interativos, buffs e debuffs dinâmicos, e a masmorra, focando na otimização para uma experiência fluida.</li>
-                                    <li className="text-clay-navy mt-3">Participei da criação dos personagens e animações detalhadas, criando uma estética sombria e uma interface de usuário intuitiva e funcional.</li>
-                                    <li className="text-clay-navy mt-3">Coordenei a equipe, integrei assets e participei ativamente de testes e ajustes, garantindo uma experiência coesa e refinada.</li>
+                                    <li className="text-clay-navy mt-3">Aprendi a aplicar conceitos de responsividade e acessibilidade, garantindo uma experiência consistente em diferentes dispositivos.</li>
+                                    <li className="text-clay-navy mt-3">Desenvolvi boas práticas de estruturação, facilitando a reutilização de componentes criados e manutenção do projeto.</li>
+                                    <li className="text-clay-navy mt-3"> Aprendi a implementar efeitos visuais diferenciados como o Parallax para melhorar a experiência do usuário.</li>
                                 </ul>
                             </div>
                         </div>
@@ -75,11 +75,7 @@ function Project3Page() {
 
 
                     <div className="p-8 flex flex-wrap justify-center items-center">
-                        <VideoShowcase
-                            url={"https://www.youtube.com/embed/AM8DZEEDCwE"}
-                            width={"560"}
-                            height={"315"}
-                        ></VideoShowcase>
+                        <img src="img/website-portfolio-image1.png" alt="" className="lg:h-2/3" />
                         <TeamMembers members={membersList}></TeamMembers>
 
                     </div>
