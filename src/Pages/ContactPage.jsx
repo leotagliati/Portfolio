e71fdd816f660f5feb1e93ad309f9ffbe54edb6a
Leonardo Tagliati <leotagliati56@gmail.com>
2025-01-14 12:31:01 -0300

    button

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,9 +1,10 @@
-import { TextField } from "@mui/material";
-import TextCalloutArea from "../components/ContactPage/TextCalloutArea";
-
+import { Button, TextField } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
+import MenuBar from "../components/ProjectPage/MenuBar";
 function ContactPage() {
     return (
         <div className="bg-red-400 h-screen">
+            <MenuBar></MenuBar>
             <div className="flex justify-end">
                 <div className="bg-green-400 absolute my-24 h-160 w-3/6 mx-12 z-1">
                     <div className="bg-blue-300 relative">
@@ -12,15 +13,18 @@ function ContactPage() {
                             <TextField id="standard-basic" label="Nome Completo" variant="standard" className="w-full" />
                             <TextField id="standard-basic" label="Email" variant="standard" className="w-full" />
                             {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" maxRows={3} className="w-full "></TextField> */}
-                            <TextField id="filled-multiline-static" label="Mensagem" multiline rows={8} placeholder="Digite sua mensagem" variant="filled" className="w-full"
-                            />
+                            <TextField id="filled-multiline-static" label="Mensagem" multiline rows={8} placeholder="Digite sua mensagem" variant="filled" className="w-full" />
+                            <div className="flex justify-end">
+                                <Button variant="contained" endIcon={<SendIcon></SendIcon>}
+                                >Enviar</Button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="flex flex-row z-0">
-                <div className="bg-blue-600 h-screen w-2/5"></div>
-                <div className="bg-yellow-300 h-screen w-3/5"></div>
+                <div className="bg-dark-purple-leo h-screen w-2/5"></div>
+                <div className="bg-navy-purple-leo h-screen w-3/5"></div>
             </div>
         </div>
 
